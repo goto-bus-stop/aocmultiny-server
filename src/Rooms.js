24f26b8d576608fe41077cf1fba31334121d36ca
René Kooi <renee@kooi.me>
2016-06-28 13:50:26 +0200

    use class syntax

--- src/Rooms.js
+++ src/Rooms.js
@@ -1,24 +1,24 @@
 import { v4 as uuid } from 'uuid'
 
-export function Room (name) {
-  if (!(this instanceof Room)) {
-    return new Room(name)
+export class Room {
+  constructor (name) {
+    this.id = uuid()
+    this.name = name
+    this.players = []
   }
-  this.id = uuid()
-  this.name = name
-  this.players = []
-}
 
-Object.assign(Room.prototype, {
   join (id) {
     this.players = this.players.concat([ id ])
-  },
+  }
+
   leave (id) {
     this.players = this.players.filter((player) => player.id !== id)
-  },
+  }
+
   empty () {
     return this.players.length === 0
-  },
+  }
+
   toJSON () {
     return {
       id: this.id,
@@ -26,33 +26,33 @@ Object.assign(Room.prototype, {
       players: this.players
     }
   }
-})
-
-export default function Rooms () {
-  if (!(this instanceof Rooms)) {
-    return new Rooms()
-  }
-
-  this.rooms = {}
 }
 
-Object.assign(Rooms.prototype, {
+export default class Rooms {
+  constructor () {
+    this.rooms = {}
+  }
+
   find (id) {
     return Promise.resolve(this.rooms[id])
-  },
+  }
+
   async findByPlayer (id) {
     const rooms = await this.all()
     return rooms.find((room) => room.players.includes(id))
-  },
+  }
+
   all () {
     return Promise.resolve(
       Object.keys(this.rooms).map((id) => this.rooms[id])
     )
-  },
+  }
+
   create ({ name }) {
     const room = new Room(name)
     return this.save(room)
-  },
+  }
+
   save (room) {
     if (!(room instanceof Room)) {
       return Promise.reject(new TypeError('Can only save instances of Room'))
@@ -60,4 +60,4 @@ Object.assign(Rooms.prototype, {
     this.rooms[room.id] = room
     return Promise.resolve(room)
   }
-})
+}
